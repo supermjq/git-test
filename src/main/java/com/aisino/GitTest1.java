083a853b623cce8e132719fd1ac880f819968a32
mjq <mjq@aisino.com>
2021-06-02 09:45:29 +0800

    three commit

--- src/main/java/com/aisino/GitTest1.java
+++ src/main/java/com/aisino/GitTest1.java
@@ -11,5 +11,6 @@ public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("hello git1");
         System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }
