aff69352af0a6f96806fa8a61ccc5f0a6901f257
mjq <mjq@aisino.com>
2021-06-02 09:37:45 +0800

    first commit

---
+++ src/main/java/com/aisino/GitTest.java
@@ -0,0 +1,15 @@
+package com.aisino;
+
+/**
+ * @author
+ * @packageName:com.aisino
+ * @ClassName:GitTest
+ * @Description:
+ * @date 2021/6/2 8:58
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("git test");
+    }
+}
---
+++ src/main/java/com/aisino/GitTest1.java
@@ -0,0 +1,14 @@
+package com.aisino;
+
+/**
+ * @author
+ * @packageName:com.aisino
+ * @ClassName:GitTest1
+ * @Description:
+ * @date 2021/6/2 9:04
+ */
+public class GitTest1 {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+    }
+}
