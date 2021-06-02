4372fb9cfc8482d54d5296118e108cd6a025fc57
mjq <mjq@aisino.com>
2021-06-02 10:15:12 +0800

    five commit

--- src/main/java/com/aisino/GitTest1.java
+++ src/main/java/com/aisino/GitTest1.java
@@ -11,6 +11,6 @@ public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("hello git1");
         System.out.println("hello git2");
-        System.out.println("hello git3");
+        System.out.println("hello git5");
     }
 }
